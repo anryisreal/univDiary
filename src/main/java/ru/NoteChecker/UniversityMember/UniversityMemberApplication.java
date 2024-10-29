b27128bd6b36707bae883fd4e6079f7e2e81934d
anryisreal <Krutt228zz@mail.ru>
2024-10-29 21:28:18 +0300

    Create project

---
+++ src/main/java/ru/NoteChecker/UniversityMember/UniversityMemberApplication.java
@@ -0,0 +1,13 @@
+package ru.NoteChecker.UniversityMember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityMemberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/NoteChecker/UniversityMember/UniversityMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.NoteChecker.UniversityMember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
